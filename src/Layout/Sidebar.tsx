bfb0f23d82687bc648cfd5f139cc1bd9b1ff3a17
Habibul Islam <habibulislam6862@gmail.com>
2023-11-25 08:31:27 +0600

    General Context Menu at Layout's new button is added

--- src/Layout/Sidebar.tsx
+++ src/Layout/Sidebar.tsx
@@ -1,13 +1,30 @@
-import {Button} from "react-bootstrap";
-import {BiPlus} from "react-icons/bi";
+import { Button } from "react-bootstrap";
+import { BiPlus } from "react-icons/bi";
+import GeneralContextMenu from "../components/GeneralContextMenu/GeneralContextMenu.tsx";
+import { useContextMenu } from "react-contexify";
+import { MouseEvent } from "react";
 
 const Sidebar = () => {
+    const { show } = useContextMenu({
+        id: "GeneralContextMenu",
+    });
+
+    const displayGeneralContextMenu = (event: MouseEvent<HTMLElement>) => {
+        show({ event });
+    };
     return (
         <div>
-            <Button className={'bg-white px-4 py-2 shadow-sm rounded-4'} variant={'light'}>
-                <span style={{fontSize: '25px'}}><BiPlus/></span>
+            <Button
+                onClick={displayGeneralContextMenu}
+                className={"bg-white px-4 py-2 shadow-sm rounded-4"}
+                variant={"light"}
+            >
+                <span style={{ fontSize: "25px" }}>
+                    <BiPlus />
+                </span>
                 New
             </Button>
+            <GeneralContextMenu id={"GeneralContextMenu"} />
         </div>
     );
 };
